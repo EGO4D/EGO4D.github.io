0bd04a1acbc76e2aad0e8f2d58dac4d21d06c333
EGO4D <87500247+EGO4D@users.noreply.github.com>
2021-09-18 16:41:19 +0100

    Updated form submission with request status
    More meaningful messages for request status

--- assets/scripts/form-submission.js
+++ assets/scripts/form-submission.js
@@ -18,13 +18,15 @@ $(document).ready(function() {
         $('#submit-btn').prop('disabled', true);
         $('#submit-btn').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>  Document requested.</button>');
 
-        if (validateEmail(email)) {
-            timestamp.setUTCSeconds(utcSeconds);
-        
+        if (validateEmail(email)) {        
+            var rerequest = false;
+            if ($('#submit-btn').text === 'Didn\'t receive an email? Request Again.') {
+                rerequest = true;
+            }
+            
             var data = JSON.stringify({
                 'email': email,
-                'type': "FORM REQUEST",
-                'optTimestamp': timestamp.toString()
+                'rerequest': rerequest
             });
 
             console.log(data);
@@ -37,8 +39,23 @@ $(document).ready(function() {
                 contentType: 'application/json',
                 data: data,
                 success: function(res) {
-                    responseTextElement.html('<div class="mt-3 alert alert-success" role="alert">Document request successful. Please check your email.</div>');
-                    $('#submit-btn').text('Didn\'t receive an email? Request Again.');
+                    var requestStatus = res.headers["Request-Status"];
+                    if (requestStatus === "New") {                   
+                        responseTextElement.html('<div class="mt-3 alert alert-success" role="alert">Document request successful! Please check your email including your Junk mail.</div>');
+                        $('#submit-btn').text('Didn\'t receive an email? Request Again.');
+                    } else if (requestStatus === "Requested") {                   
+                        responseTextElement.html('<div class="mt-3 alert alert-success" role="alert">A request for this email has already been made. Please check your email including your Junk mail.</div>');
+                        $('#submit-btn').text('Didn\'t receive an email? Request Again.');
+                    } else if (requestStatus === "Pending") {                    
+                        responseTextElement.html('<div class="mt-3 alert alert-success" role="alert">This licenses for this email have already been signed and are currently being processed. Please allow for up to 3 days from when the request was made to hear back regarding its status</div>');
+                    } else if (requestStatus === "Approved") {                    
+                        responseTextElement.html('<div class="mt-3 alert alert-success" role="alert">This licenses for this email have already been approved. There is no need for another request.</div>');
+                    } else if (requestStatus === "Rejected") {                    
+                        responseTextElement.html('<div class="mt-3 alert alert-danger" role="alert">This licenses for this email were recently rejected. Please allow up to 7 days since notifcation of the rejection to request again.</div>');
+                    } else if (requestStatus === "Bounced") {                    
+                        responseTextElement.html('<div class="mt-3 alert alert-danger" role="alert">We are unable to succesfully deliver to this email address. Please try again with another email address.</div>');
+                        $('#submit-btn').text('Request using a new email.');
+                    }
                     $('#submit-btn').prop('disabled', false);
                     console.log(res);
                 },
